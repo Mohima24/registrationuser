69339b532316a31e3837c7efe4c703b69249d185
unknown <mohimabahadur@gmail.com>
2023-06-23 19:42:48 +0530

    hello

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require("dotenv").config()
 
 mongoose.set('strictQuery', false)
-const connection = mongoose.connect(process.env.mongoUrl)
+const connection = mongoose.connect(`${process.env.mongoUrl}`)
 
 module.exports={
     connection
